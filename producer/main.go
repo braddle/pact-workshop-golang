e5b6d5e9e64488a45fc444fd26b1318f1e1bade9
Mark Bradley <braddle@gmail.com>
2022-03-11 09:40:55 +0000

    Creating GET /thing API to test against as part of the lab work

--- producer/main.go
+++ producer/main.go
@@ -16,6 +16,30 @@ type HealthCheck struct {
 	Boolean bool    `json:"boolean"`
 }
 
+type JsonError struct {
+	Code    int    `json:"code"`
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
+type Thing struct {
+	ID            string         `json:"id"`
+	Name          string         `json:"name"`
+	Integer       int64          `json:"integer"`
+	Float         float64        `json:"float"`
+	Boolean       bool           `json:"boolean"`
+	SmallerThing  SmallerThing   `json:"smaller_thing"`
+	Strings       []string       `json:"strings"`
+	Integers      []int64        `json:"integers"`
+	floats        []float64      `json:"floats"`
+	SmallerThings []SmallerThing `json:"smaller_things"`
+}
+
+type SmallerThing struct {
+	Title string `json:"title"`
+	Age   int64  `json:"age"`
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
@@ -30,6 +54,51 @@ func main() {
 		w.Header().Add("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(hc)
+	})
+	r.HandleFunc("/thing/{id}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		if id == "123456789" {
+			e := JsonError{
+				Code:    http.StatusNotFound,
+				Status:  "NotFound",
+				Message: "Could not find thing with id: 123456789",
+			}
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(e)
+			return
+		}
+
+		t := Thing{
+			ID:      "987654321",
+			Name:    "Testing",
+			Integer: 1357,
+			Float:   4.56,
+			Boolean: false,
+			SmallerThing: SmallerThing{
+				Title: "",
+				Age:   0,
+			},
+			Strings:  []string{"one", "two", "three"},
+			Integers: []int64{1, 2, 3, 4},
+			floats:   []float64{1.23, 3.45, 5.67},
+			SmallerThings: []SmallerThing{
+				{
+					Title: "First",
+					Age:   3,
+				},
+				{
+					Title: "Second",
+					Age:   2,
+				},
+				{
+					Title: "Third",
+					Age:   1,
+				},
+			},
+		}
 	})
 
 	log.Info("Server Stating")

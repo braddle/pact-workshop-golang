8f96d6875cc81f6bbcbb5596c36283577698fb72
Mark Bradley <braddle@gmail.com>
2022-07-07 10:17:19 +0100

    Re-arranging the code to move the most important parts to the top of the file.

--- consumer/health/health.go
+++ consumer/health/health.go
@@ -7,26 +7,13 @@ import (
 	"net/http"
 )
 
-type ResponseWithPactExamples struct {
-	// pact tags are used to configure example response values to be used in tests
-	Status  string  `json:"Status" pact:"example=NOT OK"`
-	Integer int64   `json:"integer" pact:"example=36"`
-	Float   float64 `json:"float" pact:"example=12.34"`
-	Boolean bool    `json:"boolean" pact:"example=false"`
+// NewChecker creates an instance of a Healthcheck please provide the hostname for the service providing the /health
+// endpoint (Example: http://api.testing.com)
+func NewChecker(hostname string) HealthChecker {
+	return HealthChecker{hostname}
 }
 
-type ResponseWithoutPactExamples struct {
-	Status  string  `json:"Status"`
-	Integer int64   `json:"integer"`
-	Float   float64 `json:"float"`
-	Boolean bool    `json:"boolean"`
-}
-
-type HealthChecker struct {
-	host string
-}
-
-// Check makes a HTTP call to the Producers healthcheck endpoint /health
+// Check makes an HTTP call to the Producers healthcheck endpoint /health
 func (c HealthChecker) Check(ctx context.Context) ResponseWithPactExamples {
 	// Creating the URL to request provide host's /health endpoint
 	url := fmt.Sprintf("%s/health", c.host)
@@ -51,8 +38,21 @@ func (c HealthChecker) Check(ctx context.Context) ResponseWithPactExamples {
 	return hr
 }
 
-// NewChecker creates an instance of a Healthcheck please provide the hostname for the service providing the /health
-// endpoint (Example: http://api.testing.com)
-func NewChecker(hostname string) HealthChecker {
-	return HealthChecker{hostname}
+type ResponseWithPactExamples struct {
+	// pact tags are used to configure example response values to be used in tests
+	Status  string  `json:"Status" pact:"example=NOT OK"`
+	Integer int64   `json:"integer" pact:"example=36"`
+	Float   float64 `json:"float" pact:"example=12.34"`
+	Boolean bool    `json:"boolean" pact:"example=false"`
+}
+
+type ResponseWithoutPactExamples struct {
+	Status  string  `json:"Status"`
+	Integer int64   `json:"integer"`
+	Float   float64 `json:"float"`
+	Boolean bool    `json:"boolean"`
+}
+
+type HealthChecker struct {
+	host string
 }
